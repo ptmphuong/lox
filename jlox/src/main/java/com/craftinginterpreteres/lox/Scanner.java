6839fc51c7c049b5de2309f29b2eccc2bcbdd463
ptmphuong <jennyhiding@gmail.com>
2022-04-14 18:47:22 -0700

    implemented scanner

--- jlox/src/main/java/com/craftinginterpreteres/lox/Scanner.java
+++ jlox/src/main/java/com/craftinginterpreteres/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreteres.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Scan for the lexeme, consumes it and any following characters that are part of it.
@@ -18,6 +20,28 @@ public class Scanner {
   private int start = 0; // the first character in the lexeme being scanned
   private int current = 0; // the character currently being considered
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    TokenType.AND);
+    keywords.put("class",  TokenType.CLASS);
+    keywords.put("else",   TokenType.ELSE);
+    keywords.put("false",  TokenType.FALSE);
+    keywords.put("for",    TokenType.FOR);
+    keywords.put("fun",    TokenType.FUN);
+    keywords.put("if",     TokenType.IF);
+    keywords.put("nil",    TokenType.NIL);
+    keywords.put("or",     TokenType.OR);
+    keywords.put("print",  TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super",  TokenType.SUPER);
+    keywords.put("this",   TokenType.THIS);
+    keywords.put("true",   TokenType.TRUE);
+    keywords.put("var",    TokenType.VAR);
+    keywords.put("while",  TokenType.WHILE);
+  }
 
   public Scanner(String source) {
     this.source = source;
@@ -59,7 +83,7 @@ public class Scanner {
         addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
         break;
       case '>':
-        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.EQUAL);
+        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
         break;
       case '/':
         if (match('/')) {
@@ -69,15 +93,67 @@ public class Scanner {
           addToken(TokenType.SLASH);
         }
         break;
-
       case ' ': case '\r': case 't': break;
-
       case '\n': line++; break;
 
+      // LITERALS
+      case '"': string(); break;
+
       default:
-        Lox.error(line, "Unexpected character,");
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) { // RESERVED WORDS & IDENTIFIERS - maximal munch principle
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character,");
+        }
         break;
     }
+  }
+
+
+  private void identifier() {
+    while (isAlphanumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.getOrDefault(text, TokenType.IDENTIFIER);
+    addToken(type);
+  }
+
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance(); // consume the "."
+      while (isDigit(peek())) advance();
+    }
+
+    String value = source.substring(start, current);
+    addToken(TokenType.NUMBER, Double.parseDouble(value));
+  }
+
+
+  /**
+   * scan & consume a string literal
+   * add it to the tokens list if it's a valid string literal.
+   */
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance(); // consume the closing ".
+
+
+    String value = source.substring(start+1, current-1);
+    addToken(TokenType.STRING, value);
   }
 
 
@@ -101,6 +177,31 @@ public class Scanner {
   }
 
 
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+
+  private boolean isAlpha(char c) {
+    return (
+        (c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z')
+        || c == '_'
+        );
+  }
+
+
+  private boolean isAlphanumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+
   private char advance() {
     return source.charAt(current++);
   }
@@ -111,9 +212,9 @@ public class Scanner {
   }
 
 
-  private void addToken(TokenType type, Object liternal) {
+  private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);
-    tokens.add(new Token(type, text, liternal, line));
+    tokens.add(new Token(type, text, literal, line));
   }
 
 
